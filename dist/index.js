89b421c2959fd73b403b4d5220f444e009459d5e
Dylan Rinker <dylan.p.rinker@gmail.com>
2023-07-30 23:58:07 -0400

    m

--- dist/index.js
+++ dist/index.js
@@ -40,55 +40,53 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(817);
+// import {Octokit} from '@octokit/action'
+// import {wait} from './wait'
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            core.info('Hello world, again!');
         }
         catch (error) {
             if (error instanceof Error)
                 core.setFailed(error.message);
         }
+        core.summary
+            .addHeading('Summary Heading')
+            .addLink('GitHub', 'https://github.com')
+            .write();
     });
 }
+// async function run2(): Promise<void> {
+//   try {
+//     // if token is github token and organization isn't the current org, throw an error
+//     const copilotOrganization: string = core.getInput('organization') || (process.env.GITHUB_REPOSITORY || '').split('/')[0];
+//     core.debug(`Organization: ${copilotOrganization}`);
+//     const currentOrganization: string = (process.env.GITHUB_REPOSITORY || '').split('/')[0];
+//     const daysToDeallocate: number = parseInt(core.getInput('daysToDeallocate'));
+//     core.debug(`Days to deallocate: ${daysToDeallocate}`);
+//     // if (process.env.GITHUB_TOKEN && copilotOrganization != currentOrganization) {
+//     //   throw new Error('If using GitHub Token, organization must be the same as the current repository');
+//     // }
+//     const octokit = new Octokit();
+//     const {data} = await octokit.request('GET /organizations/{org}/copilot/billing', {
+//       org: 'octodemo',
+//       headers: {
+//         'X-GitHub-Api-Version': '2022-11-28'
+//       }
+//     });M
+//     console.log(data);
+//     // const ms: string = core.getInput('milliseconds')
+//     // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+//     // core.debug(new Date().toTimeString())
+//     // await wait(parseInt(ms, 10))
+//     // core.debug(new Date().toTimeString())
+//     // core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
 run();
-
-
-/***/ }),
-
-/***/ 817:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,59 @@
 import * as core from '@actions/core'
-const { Octokit } = require("@octokit/action");
+// import {Octokit} from '@octokit/action'
 // import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    console.log('Hello world!');
+    core.info('Hello world, again!')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
+
+  core.summary
+    .addHeading('Summary Heading')
+    .addLink('GitHub', 'https://github.com')
+    .write()
 }
 
+// async function run2(): Promise<void> {
+//   try {
 
-async function run2(): Promise<void> {
-  try {
+//     // if token is github token and organization isn't the current org, throw an error
 
-    // if token is github token and organization isn't the current org, throw an error
+//     const copilotOrganization: string = core.getInput('organization') || (process.env.GITHUB_REPOSITORY || '').split('/')[0];
+//     core.debug(`Organization: ${copilotOrganization}`);
 
+//     const currentOrganization: string = (process.env.GITHUB_REPOSITORY || '').split('/')[0];
 
+//     const daysToDeallocate: number = parseInt(core.getInput('daysToDeallocate'));
+//     core.debug(`Days to deallocate: ${daysToDeallocate}`);
 
+//     // if (process.env.GITHUB_TOKEN && copilotOrganization != currentOrganization) {
+//     //   throw new Error('If using GitHub Token, organization must be the same as the current repository');
+//     // }
 
+//     const octokit = new Octokit();
 
+//     const {data} = await octokit.request('GET /organizations/{org}/copilot/billing', {
+//       org: 'octodemo',
+//       headers: {
+//         'X-GitHub-Api-Version': '2022-11-28'
+//       }
+//     });M
 
+//     console.log(data);
 
-    const copilotOrganization: string = core.getInput('organization') || (process.env.GITHUB_REPOSITORY || '').split('/')[0];
-    core.debug(`Organization: ${copilotOrganization}`);
+//     // const ms: string = core.getInput('milliseconds')
+//     // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    const currentOrganization: string = (process.env.GITHUB_REPOSITORY || '').split('/')[0];
+//     // core.debug(new Date().toTimeString())
+//     // await wait(parseInt(ms, 10))
+//     // core.debug(new Date().toTimeString())
 
-    const daysToDeallocate: number = parseInt(core.getInput('daysToDeallocate'));
-    core.debug(`Days to deallocate: ${daysToDeallocate}`);
-
-    
-    // if (process.env.GITHUB_TOKEN && copilotOrganization != currentOrganization) {
-    //   throw new Error('If using GitHub Token, organization must be the same as the current repository');
-    // }
-
-    const octokit = new Octokit();
-
-    const {data} = await octokit.request('GET /organizations/{org}/copilot/billing', {
-      org: 'octodemo',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    });
-
-    console.log(data);
-
-
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
-
-    // core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
-}
+//     // core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
 
 run()
